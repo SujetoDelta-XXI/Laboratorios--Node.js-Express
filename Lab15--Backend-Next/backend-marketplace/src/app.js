62ce11ba201c61d12947a4a7228ae8cb20f51055
Carlos Asparrin Martin <150694554+SujetoDelta-XXI@users.noreply.github.com>
2025-11-23 19:25:27 -0500

    .

--- Lab15--Backend-Next/backend-marketplace/src/app.js
+++ Lab15--Backend-Next/backend-marketplace/src/app.js
@@ -9,42 +9,28 @@ const adminRouter = require('./routes/admin');
 
 const app = express();
 
-/*CORS CONFIGURADO PARA PRODUCCIÓN*/
-
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  process.env.FRONTEND_ORIGIN 
-];
-
+/* CORS SEGURO PARA PRODUCCIÓN */
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('CORS bloqueado para: ' + origin));
-    }
-  },
+  origin: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://laboratorios-node-js-express.vercel.app'
+  ],
   credentials: true
 }));
 
-/*MIDDLEWARES*/
-
+/* MIDDLEWARES */
 app.use(cookieParser());
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
-/*RUTAS */
-
+/* RUTAS */
 app.use('/api/products', productsRouter);
 app.use('/api/categories', categoriesRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/admin', adminRouter);
 
-/*RUTA PRINCIPAL*/
-
+/* ROOT */
 app.get('/', (req, res) => {
   res.json({
     status: 'OK',
@@ -52,7 +38,7 @@ app.get('/', (req, res) => {
   });
 });
 
-
+/* 404 */
 app.use((req, res) => {
   res.status(404).json({ error: 'Ruta no encontrada' });
 });
